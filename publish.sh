d61405fb647903e1f7a4dec451001c92bf68dca3
13r0ck <bnr@tuta.io>
2022-11-21 07:17:27 -0700

    Use rsync to also update folders

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ if git status | grep -q "Your branch is up to date with 'origin/main'"; then
 	npm run-script build
 	cp ./dist /tmp/dist -r
 	git checkout workers
-	mv /tmp/dist/* ./public/
+	rsync -a /tmp/dist/ ./public/
 	git add -A
 	git commit -m "Update for $main_hash"
 	git push origin workers
